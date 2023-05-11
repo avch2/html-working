12ab95fedebff6bac5e50fc98c8578cd6f5ffe9a
avch <avch2@ukr.net>
2023-05-11 22:15:41 +0300

    +comment

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { // обработчик событий дожидается загрузки ДОМ и потом запускает скрипт
     const movieDB = {
         movies: [
             "Логан",
@@ -29,17 +29,17 @@ document.addEventListener('DOMContentLoaded', () => {
           poster = document.querySelector('.promo__bg'),
           genre = poster.querySelector('.promo__genre'),
           movieList = document.querySelector('.promo__interactive-list'),
-          addForm = document.querySelector('form.add'),
-          addInput = addForm.querySelector('.adding__input'),
-          checkbox = addForm.querySelector('[type="checkbox"]');
+          addForm = document.querySelector('form.add'),// добавляем данные в форму
+          addInput = addForm.querySelector('.adding__input'),//заходим в класс .adding__input внутри формы
+          checkbox = addForm.querySelector('[type="checkbox"]');//для определения любимого фильма использую [] - указатель атрибуа для чекбокс
 
-    addForm.addEventListener('submit', (event) => {
+    addForm.addEventListener('submit', (event) => {// submit - обработчик события, event - объект события, омена действия по умолчанию не даёт перезагрузку страницы
         event.preventDefault();
 
-        let newFilm = addInput.value;
-        const favorite = checkbox.checked;
+        let newFilm = addInput.value;// запись в переменную введенного названия
+        const favorite = checkbox.checked;//запись в переменную значения из чекбокса "любимый фильм"
 
-        if (newFilm){
+        if (newFilm){ //условие для борьбы с пустой строкой, длинной строкой и вывод в консоль надписи "Добавляем любимый фильм"
 
             if (newFilm.length > 21) {
                 newFilm = `${newFilm.substring(0, 22)}...`;
@@ -49,18 +49,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 console.log("Добавляем любимый фильм");
             }
 
-            movieDB.movies.push(newFilm);
-            sortArr(movieDB.movies);
+            movieDB.movies.push(newFilm); //добавляем новый фильм в массив
+            sortArr(movieDB.movies); //сортируем начальный массив фильмов
 
             createMovieList(movieDB.movies, movieList);
         }
+        
 
         
 
-        event.target.reset();
+        event.target.reset(); //сброс формы
     });
 
-   const deleteAdv = (arr) => {
+   const deleteAdv = (arr) => { //функция удаления рекламы
     arr.forEach(item => {
         item.remove();
     });
@@ -70,17 +71,17 @@ document.addEventListener('DOMContentLoaded', () => {
     //     item.remove();
     // });
     
-    const makeChanges = () => {
+    const makeChanges = () => { //функция для изменений на странице
         genre.textContent = 'драма';
     
         poster.style.backgroundImage = 'url("img/bg.jpg")';
     };
 
-    const sortArr = (arr) => {
+    const sortArr = (arr) => {//функция сортировки элементов массива
         arr.sort();
     };
     
-    function createMovieList(films, parent) {
+    function createMovieList(films, parent) {//films - фильмы для обработки(movieDB.movies), parent - родитель,который будет использовать эти фильмы(movieList)
         parent.innerHTML = ""; 
         sortArr(films);
     
@@ -94,12 +95,12 @@ document.addEventListener('DOMContentLoaded', () => {
             `;
         });
 
-    document.querySelectorAll('.delete').forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            btn.parentElement.remove();
-            movieDB.movies.splice(i, 1);
+    document.querySelectorAll('.delete').forEach((btn, i) => { //удаление фильма в корзину ('.delete' - корзинка в index.html)
+        btn.addEventListener('click', () => { //находим событие "клик"
+            btn.parentElement.remove(); // удаление родительского элемента со страницы
+            movieDB.movies.splice(i, 1); // вырезаем методом splice элемент массива i в количестве 1
 
-            createMovieList(films, parent);
+            createMovieList(films, parent); //запускаем рекурсию, которая заново пронумеровывает элементы вновь созданного массива
         });
     });
 
@@ -107,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     deleteAdv(adv);
     makeChanges();
-    
+    sortArr(movieDB.movies)
     createMovieList(movieDB.movies, movieList);
 
 });
